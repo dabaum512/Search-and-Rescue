f61e24356ce6e930193079e8dfbf7369e42e57ec
Justin Moser <dabaum512@gmail.com>
2015-05-05 14:12:31 -0500

    Initial Commit

---
+++ S&R/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  S&R
+//
+//  Created by Justin Moser on 5/5/15.
+//  Copyright (c) 2015 Justin Moser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ S&R/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  S&R
+//
+//  Created by Justin Moser on 5/5/15.
+//  Copyright (c) 2015 Justin Moser. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
